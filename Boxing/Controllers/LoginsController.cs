91761799db0367a74305d9bacf33de92f22b09d1
Throll <fatih_92@abv.bg>
2015-11-12 00:34:37 +0200

    Initial commit

---
+++ Boxing/Controllers/LoginsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Boxing.Controllers
+{
+    public class LoginsController : ApiController
+    {
+        public struct Login
+        {
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> Create(Login user)
+        {
+            return Request.CreateResponse(System.Net.HttpStatusCode.Created,
+                new
+                {
+                    id = 1,
+                    authToken = "token"
+                });
+        }
+
+        [HttpDelete]
+        public async Task Delete(string id)
+        {
+            
+        }
+    }
+}
---
+++ Boxing/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using Boxing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace Boxing.Controllers
+{
+    public class UsersController : ApiController
+    {
+        [HttpGet]
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            return new List<User>();
+        }
+
+        [HttpGet]
+        public async Task<HttpResponseMessage> GetById(string id)
+        {
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, new
+            {
+                username = "ddd"
+            });
+        }
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> Create(User user)
+        {
+            return Request.CreateResponse(System.Net.HttpStatusCode.Created,
+                new
+                {
+                    userId = 1,
+                    rating = 0
+                });
+        }
+    }
+}
